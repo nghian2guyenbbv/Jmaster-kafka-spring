2097eeec6639bbaabcbc811929a80af4d5252457
nghianguyenbbv <nghia2nguyen@bbv.vn>
2023-09-16 16:29:19 +0700

    update account service

--- Service/accountService/src/main/java/com/jmaster/accountService/controller/AccountController.java
+++ Service/accountService/src/main/java/com/jmaster/accountService/controller/AccountController.java
@@ -5,13 +5,11 @@ import com.jmaster.accountService.model.MessageDTO;
 import com.jmaster.accountService.model.StatictisDTO;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.kafka.core.KafkaTemplate;
-import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-import javax.annotation.PostConstruct;
 import java.util.Date;
 
 @RestController
@@ -23,7 +21,7 @@ public class AccountController {
     public AccountDTO createAcct(@RequestBody AccountDTO accountDTO){
         StatictisDTO statictisDTO = new StatictisDTO("Account"+accountDTO.getEmail()+"was created", new Date());
         MessageDTO messageDTO  = new MessageDTO();
-        messageDTO.setContent("sasaa");
+        messageDTO.setContent("hello word");
         messageDTO.setTo("nghia");
         messageDTO.setToName("Hau");
         messageDTO.setSubject("love");
